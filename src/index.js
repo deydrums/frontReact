d9f89e802486c58007f9466d2cda59adaa515201
deydrums <dagarcia100@gmail.com>
2021-07-30 19:08:57 -0600

    variables de entorno

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { FrontEnd } from './FrontEnd';
 import './styles/styles.scss';
 import 'animate.css';
+console.log(process.env.REACT_APP_API_URL);
 
 ReactDOM.render(
     <FrontEnd />,
