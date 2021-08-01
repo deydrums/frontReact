2ba89ce90a121c4d7a8e1592dba2aa8e9b7962d5
deydrums <dagarcia100@gmail.com>
2021-07-31 21:53:31 -0600

    Index

---
+++ src/components/principal/NothingScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const IndexScreen = () => {
+    return (
+        <div className ="principal__index">
+            <p>
+                Pagina de inicio
+                <br/>
+            </p>
+            <i className="far fa-star fa-4x mt-5"></i>
+        </div>
+    )
+}
--- src/components/principal/PrincipalScreen.js
+++ src/components/principal/PrincipalScreen.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Sidebar } from './Sidebar';
+import {IndexScreen} from './NothingScreen';
 
 export const PrincipalScreen = () => {
 
@@ -8,6 +9,7 @@ export const PrincipalScreen = () => {
     return (
         <div className = "principal__main-content animate__animated animate__fadeIn" >
             <Sidebar/>
+            <IndexScreen />
         </div>
     )
 }
