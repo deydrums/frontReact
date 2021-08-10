b79ec2b9d724bb8e1619c3e6c4f1f22b9df64e32
deydrums <dagarcia100@gmail.com>
2021-08-10 17:36:18 -0600

    Nueva Entrada

--- src/components/entry-blog/NewEntryScreen.js
+++ src/components/entry-blog/NewEntryScreen.js
@@ -8,7 +8,7 @@ export const NewEntryScreen = () => {
 	
 	const config = {
 		readonly: false, // all options from https://xdsoft.net/jodit/doc/
-        height: 500,
+        height: 470,
         allowResizeY: false,
         language: "es",
 	}
@@ -18,29 +18,35 @@ export const NewEntryScreen = () => {
         console.log(content)
     }
 
-    const handlec = () =>{
-        console.log(content)
-    }
+
 
     return (
         <div className="principal__content">
+            <div className="entry-blog__submit">
+                <button type="Submit" className="btn btn-primary" onClick={handleSubmit}>Guardar</button>
+            </div>
+
             <div className="entry-blog__content">
                 <div className="entry-blog__text">
                     <h3 className="principal__title"><i className="fas fa-file-alt m-2"></i> Nueva entrada</h3>
                 </div>
                 <form className="entry-blog__newEntry">
+                    <input
+                        type="text"
+                        placeholder="Titulo..."
+                        name = "title"
+                        className="auth__input"
+                        autoComplete="off"
+                    />
                     <JoditEditor
                         ref={editor}
                         value={content}
                         config={config}
                         tabIndex={1} // tabIndex of textarea
                         onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-                        onChange={handlec}
+                        //onChange={handlec}
                         className="editor"
-                        />
-
-                        <button type="Submit" className="btn btn-primary mt-2 btn-right" onClick={handleSubmit}>Enviar</button>
-                        
+                        />                        
                 </form>
             </div>
         </div>
