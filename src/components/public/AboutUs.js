1d8750633d2d4252c52c787a2fa96834db678347
deydrums <dagarcia100@gmail.com>
2021-08-12 20:19:22 -0600

    AboutUs

---
+++ src/components/public/AboutUs.js
@@ -0,0 +1,68 @@
+import React from 'react'
+
+export const AboutUs = () => {
+    return (
+        <section className="public__about-us">
+            <div className="public__container public__container__p-60">
+                <h2 className="public__title">We provide a wide range</h2>
+                <div className="public__articles">
+                    <div className="public__article" data-aos="zoom-in-right">
+                        <div className="public__icon">
+                            <i className="fas fa-pen-fancy"></i>
+                        </div>
+                        <h3>Web design</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum repudiandae eaque esse excepturi
+                            dolore necessitatibus!</p>
+                        <a href="https://www.google.com">Read more</a>
+                    </div>
+                    <div className="public__article" data-aos="zoom-in-right">
+                        <div className="public__icon">
+                            <i className="fas fa-pen-fancy"></i>
+                        </div>
+                        <h3>Graphic design</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum repudiandae eaque esse excepturi
+                            dolore necessitatibus!</p>
+                        <a href="https://www.google.com">Read more</a>
+                    </div>
+                    <div className="public__article" data-aos="zoom-in-right">
+                        <div className="public__icon">
+                            <i className="fas fa-pen-fancy"></i>
+                        </div>
+                        <h3>Video production</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum repudiandae eaque esse excepturi
+                            dolore necessitatibus!</p>
+                        <a href="https://www.google.com">Read more</a>
+                    </div>
+                    <div className="public__article" data-aos="zoom-in-right">
+                        <div className="public__icon">
+                            <i className="fas fa-pen-fancy"></i>
+                        </div>
+                        <h3>SEO</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum repudiandae eaque esse excepturi
+                            dolore necessitatibus!</p>
+                        <a href="https://www.google.com">Read more</a>
+                    </div>
+                    <div className="public__article" data-aos="zoom-in-right">
+                        <div className="public__icon">
+                            <i className="fas fa-pen-fancy"></i>
+                        </div>
+                        <h3>Marketing</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum repudiandae eaque esse excepturi
+                            dolore necessitatibus!</p>
+                        <a href="https://www.google.com">Read more</a>
+                    </div>
+                    <div className="public__article" data-aos="zoom-in-right">
+                        <div className="public__icon">
+                            <i className="fas fa-pen-fancy"></i>
+                        </div>
+                        <h3>Inphografics</h3>
+                        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum repudiandae eaque esse excepturi
+                            dolore necessitatibus!</p>
+                        <a href="https://www.google.com">Read more</a>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    )
+}
--- src/components/public/IndexScreen.js
+++ src/components/public/IndexScreen.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Header } from './Header';
+import AOS from 'aos';
+import "aos/dist/aos.css";
+import { AboutUs } from './AboutUs';
 
 export const IndexScreen = () => {
+
+
+    useEffect(() => {
+        AOS.init({
+          duration : 1000
+        });
+      }, []);
+
     return (
         <>
             <Header/>
+            <AboutUs/>
         </>
         
     )
