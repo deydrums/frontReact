ccf7a0ea52298a45dd1a64a19600f1e05b942fcc
deydrums <dagarcia100@gmail.com>
2021-08-13 20:00:13 -0600

    Obtener entradas

--- src/components/public/blog/BlogScreen.js
+++ src/components/public/blog/BlogScreen.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { images } from '../../../helpers/getImages';
 import { AboutUs } from '../AboutUs';
+import { useDispatch } from 'react-redux';
+import { startGetEntries } from '../../../actions/blog';
 
 export const BlogScreen = () => {
+    const dispatch = useDispatch();
+    dispatch(startGetEntries());
+
     return (
         <>
         <div className="public__blog-header">
