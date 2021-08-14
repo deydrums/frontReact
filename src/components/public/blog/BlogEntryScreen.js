4949db42e8b7f1185879f51a488f72252b4243e0
deydrums <dagarcia100@gmail.com>
2021-08-14 14:21:24 -0600

    EntryScreen

--- src/components/public/blog/BlogEntryScreen.js
+++ src/components/public/blog/BlogEntryScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { startGetEntry } from '../../../actions/blog';
+import ReactHtmlParser from 'react-html-parser';
+import { HeaderBlog } from './HeaderBlog';
 
 export const BlogEntryScreen = ({match}) => {
 
@@ -16,7 +18,19 @@ export const BlogEntryScreen = ({match}) => {
 
     return (
         <>
-            HOLA
+            <HeaderBlog/>
+            <div className="public__blog-entries">
+                <div className="public__blog-entries-head">
+                    <div className="public__blog-entries-head-cont">
+                        <div className="public__blog-entries-head-txt">
+                            <h1>Blog <i className="fas fa-angle-right"></i> { publicActiveEntry && publicActiveEntry.title}</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="public__entry-content-container">
+                { publicActiveEntry && ReactHtmlParser(publicActiveEntry.content) }
+            </div>
         </>
     )
 }
