fa9513dc51ced44de385bd753fc317d70cc9b5aa
deydrums <dagarcia100@gmail.com>
2021-08-13 20:02:14 -0600

    BlogScreen

---
+++ src/components/public/blog/HeaderBlog.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { images } from '../../../helpers/getImages';
+
+export const HeaderBlog = () => {
+    return (
+        <div className="public__blog-header">
+            <div className="public__container">
+                <section className="public__blog-contenido-header">
+                    <section className="public__blog-textos-header">
+                        <h1 className="title">Blog</h1>
+                    </section>
+                    <img src={images(`./blog.svg`).default} alt=""/>
+                </section>
+            </div>
+        </div>
+    )
+}
