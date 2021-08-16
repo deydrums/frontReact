9e29d29edad262b6e48d6c3652b3d3489e7cc091
deydrums <dagarcia100@gmail.com>
2021-08-16 17:11:04 -0600

    PublicRoute

--- src/routers/PublicRoutes.js
+++ src/routers/PublicRoutes.js
@@ -15,7 +15,7 @@ export const PublicRoutes = () => {
                         <Switch>
                             <Route exact path="/" component={IndexScreen}/>
                             <Route exact path="/blog" component={BlogScreen}/>
-                            <Route exact path="/blog/entrada/:id/:title" component={BlogEntryScreen}/>
+                            <Route exact path="/blog/entrada/:id/:title?" component={BlogEntryScreen}/>
                             <Redirect to="/"/>
                         </Switch>
                     </div>
