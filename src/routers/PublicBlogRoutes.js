aef6978f962b50efb91a218a51b054917b60a492
deydrums <dagarcia100@gmail.com>
2021-08-18 08:27:51 -0600

    Nav

--- src/routers/PublicBlogRoutes.js
+++ src/routers/PublicBlogRoutes.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Link, Redirect, Route, Switch } from 'react-router-dom'
+import { Link, NavLink, Redirect, Route, Switch } from 'react-router-dom'
 import { BlogEntryScreen } from '../components/public/blog/BlogEntryScreen'
 import { BlogScreen } from '../components/public/blog/BlogScreen'
 import { HeaderBlog } from '../components/public/blog/HeaderBlog'
 
 
 export const PublicBlogRoutes = ({location}) => {
-    
+
+    const onClickButtonMore = () => {
+        const enlacesHeader = document.querySelector('.public__blog-nav-links');
+        enlacesHeader.classList.toggle("show_blog-nav-links");
+    }
 
     return (
         <>  
@@ -14,16 +18,30 @@ export const PublicBlogRoutes = ({location}) => {
             <div className="public__blog-entries">
                 <div className="public__blog-nav">
                     <div className="public__blog-nav-btn">
-                        <i className="fas fa-chevron-circle-down pointer"></i>
+                        <i className="fas fa-chevron-circle-down pointer" onClick={onClickButtonMore}></i>
                     </div>
                     <div className="public__blog-nav-links">
+                    <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
+                        <NavLink className= "public__blog-nav-links-link" to = "/blog" >Blog</NavLink>
 
                     </div>
                 </div>
                 <div className="public__blog-entries-head">
                     <div className="public__blog-entries-head-cont">
                         <div className="public__blog-entries-head-txt">
-                            <h1><Link className= "public__blog__links" to = "/Blog" >Blog</Link> <i className="fas fa-angle-right"></i> Ultimas Entradas</h1>
+                            <h1><Link className= "public__blog__links" to = "/blog" >Blog</Link> <i className="fas fa-angle-right"></i> Ultimas Entradas</h1>
                         </div>
                     </div>
                 </div>
