3787820a36c4abaace5b95a9e49ba7c0cbcb97c8
deydrums <dagarcia100@gmail.com>
2021-08-19 20:53:42 -0600

    public header

--- src/components/public/HeaderScreen.js
+++ src/components/public/HeaderScreen.js
@@ -6,13 +6,18 @@ export const HeaderScreen = () => {
         <header>
             <div className="public__container">
                 <section className="public__contenido-header">
-                    <section className="public__textos-header">
-                        <h1 className="public__title">Our creativity is your success</h1>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia officia similique earum,
-                            molestias quia inventore?</p>
-                        <a href="https://www.google.com">Lear more</a>
-                    </section>
-                    <img src={images(`./soundwhite.svg`).default} alt=""/>
+                    <div className="public__header-text">
+                        <p>Hola, soy</p>
+                        <h1>David García</h1>
+                        <h2>Estudiante de Ingenieria Electrónica<br/>& desarrollador web</h2>
+                        <a href="#about" className="btn link-item">Sobre mí</a>
+                        <a href="#portafolio" className="btn link-item">Portafolio</a>
+                    </div>
+                    <div className="public__header-img animate__animated animate__fadeIn">
+                        <div className="img-box">
+                            <img src={images(`./profile-img.png`).default}alt="profile-img"/>
+                        </div>
+                    </div>
                 </section>
             </div>
         </header>
